df9e96d8ffb21594a28a859269a337f2ff354112
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-24 15:59:04 -0500

    fix(codegen): change to List of JsonElement global_constants because currently is unsupported

--- godot-java-codegen/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensionapi/ExtensionApi.kt
+++ godot-java-codegen/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensionapi/ExtensionApi.kt
@@ -2,6 +2,7 @@ package io.github.kingg22.godot.codegen.models.extensionapi
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 
 @Serializable
 class ExtensionApi(
@@ -10,7 +11,7 @@ class ExtensionApi(
     @SerialName("builtin_class_member_offsets") val builtinClassMemberOffsets: List<BuiltinClassMemberOffsets>,
     @SerialName("builtin_classes") val builtinClasses: List<BuiltinClass>,
     val classes: List<GodotClass>,
-    @SerialName("global_constants") val globalConstants: List<String>,
+    @SerialName("global_constants") val globalConstants: List<JsonElement>,
     @SerialName("global_enums") val globalEnums: List<ApiEnum>,
     @SerialName("utility_functions") val utilityFunctions: List<UtilityFunction>,
     @SerialName("native_structures") val nativeStructures: List<NativeStructure>,
