18a3ef844ebbb2098a8d6e6af039dd385a92b38c
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-28 22:42:36 -0500

    perf(codegen): remove build singleton class of builtin stub generator and misc
    replace inefficient check of companion filled splitting methods (static or not)

--- codegen/src/main/kotlin/io/github/kingg22/godot/codegen/impl/extensionapi/stubs/BuiltinStubGenerator.kt
+++ codegen/src/main/kotlin/io/github/kingg22/godot/codegen/impl/extensionapi/stubs/BuiltinStubGenerator.kt
@@ -1,18 +1,10 @@
 package io.github.kingg22.godot.codegen.impl.extensionapi.stubs
 
-import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.KModifier
-import com.squareup.kotlinpoet.MemberName
-import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.UNIT
 import io.github.kingg22.godot.codegen.impl.commonConfiguration
-import io.github.kingg22.godot.codegen.impl.extensionapi.Context
 import io.github.kingg22.godot.codegen.impl.extensionapi.TypeResolver
-import io.github.kingg22.godot.codegen.impl.jvmStaticAnnotation
 import io.github.kingg22.godot.codegen.impl.renameGodotClass
 import io.github.kingg22.godot.codegen.impl.withExceptionContext
 import io.github.kingg22.godot.codegen.models.extensionapi.BuiltinClass
@@ -27,77 +19,52 @@ class BuiltinStubGenerator(
 ) {
     private val methodGen = MethodStubGenerator(packageName, typeResolver)
 
-    context(context: Context)
     fun generate(cls: BuiltinClass): FileSpec? {
         if (cls.name.lowercase() in MAPPED_GODOT_BUILTIN_CLASSES) return null
-        val isSingleton = context.isSingleton(cls)
         val className = cls.name.renameGodotClass()
 
         return withExceptionContext({ "Generating builtin class '$className'" }) {
-            val typeBuilder = if (isSingleton) {
-                buildSingletonClass(className)
-            } else {
-                TypeSpec.Companion.classBuilder(className).addModifiers(KModifier.OPEN)
+            val typeBuilder = TypeSpec.classBuilder(className)
+
+            val companionBuilder = TypeSpec.companionObjectBuilder()
+
+            val (staticMethods, instanceMethods) = cls.methods.partition { it.isStatic }
+
+            fun buildMethod(method: BuiltinClass.BuiltinMethod) = withExceptionContext({
+                "Error generating method '${method.name}', return: ${method.returnType}"
+            }) {
+                val returnType = method.returnType?.let { typeResolver.resolve(it) } ?: UNIT
+                methodGen.generate(
+                    name = method.name,
+                    returnType = returnType,
+                    returnTypeString = method.returnType,
+                    isOpen = false,
+                    arguments = method.arguments,
+                )
             }
 
-            val companionBuilder = typeBuilder.typeSpecs
-                .find { it.isCompanion }
-                ?.also { typeBuilder.typeSpecs.remove(it) }
-                ?.toBuilder()
-                ?: TypeSpec.Companion.companionObjectBuilder()
-
-            cls.methods.forEach { method ->
-                withExceptionContext({ "Error generating method '${method.name}', return: ${method.returnType}" }) {
-                    val returnType = method.returnType?.let { typeResolver.resolve(it) } ?: UNIT
-                    val funSpec = methodGen.generate(
-                        name = method.name,
-                        returnType = returnType,
-                        returnTypeString = method.returnType,
-                        isOpen = !isSingleton && !method.isStatic,
-                        arguments = method.arguments,
-                    )
-                    if (method.isStatic) {
-                        companionBuilder.addFunction(funSpec.addAnnotation(jvmStaticAnnotation()).build())
-                    } else {
-                        typeBuilder.addFunction(funSpec.build())
-                    }
+            for (staticMethod in staticMethods) {
+                buildMethod(staticMethod).build().also {
+                    companionBuilder.addFunction(it)
                 }
             }
 
-            if (companionBuilder.build() != TypeSpec.Companion.companionObjectBuilder().build()) {
-                typeBuilder.typeSpecs.addFirst(companionBuilder.build())
+            for (instanceMethod in instanceMethods) {
+                typeBuilder.addFunction(buildMethod(instanceMethod).build())
+            }
+
+            if (staticMethods.isNotEmpty()) {
+                typeBuilder.addType(companionBuilder.build())
             }
 
             cls.enums.map { enumGen.generateSpec(it) }.forEach { typeBuilder.addType(it) }
 
-            FileSpec.Companion.builder(packageName, className)
+            FileSpec
+                .builder(packageName, className)
                 .commonConfiguration()
                 .addType(typeBuilder.build())
                 .build()
         }
-    }
-
-    private fun buildSingletonClass(className: String): TypeSpec.Builder {
-        val classType = ClassName(packageName, className)
-        val lazyMethod = MemberName("kotlin", "lazy")
-        val lazyMode = ClassName("kotlin", "LazyThreadSafetyMode")
-        val companion = TypeSpec.Companion.companionObjectBuilder()
-            .addProperty(
-                PropertySpec.Companion.builder("instance", classType)
-                    .delegate(
-                        CodeBlock.Companion.builder()
-                            .beginControlFlow("%M(%T.NONE)", lazyMethod, lazyMode)
-                            .addStatement("%T()", classType)
-                            .endControlFlow()
-                            .build(),
-                    )
-                    .build(),
-            )
-            .build()
-        return TypeSpec.Companion.classBuilder(classType)
-            .addModifiers(KModifier.OPEN)
-            .primaryConstructor(FunSpec.Companion.constructorBuilder().addModifiers(KModifier.PROTECTED).build())
-            .addType(companion)
     }
 
     companion object {
