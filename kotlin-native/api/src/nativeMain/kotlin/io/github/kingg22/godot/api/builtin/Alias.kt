394459fa09cff90ca4381a7d0e723f4871e265df
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-03 22:27:34 -0500

    perf(kotlin native api): add typealias for uppercase classes
    Missing typealias MSAA = Msaa because it's inside the Viewport class as enum class, when kotlinpoet release nested typealias support, going to be added

---
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/builtin/Alias.kt
@@ -0,0 +1,5 @@
+package io.github.kingg22.godot.api.builtin
+
+public typealias AABB = Aabb
+
+public typealias RID = Rid
---
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/singleton/Alias.kt
@@ -0,0 +1,5 @@
+package io.github.kingg22.godot.api.singleton
+
+public typealias IP = Ip
+
+public typealias OS = Os
