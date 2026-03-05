2b9993aa04bd506b21f187b7db181c1752d6c0c0
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-05 13:50:53 -0500

    perf(codegen): pass utility function to method generator directly

--- codegen/src/main/kotlin/io/github/kingg22/godot/codegen/impl/extensionapi/native/generators/NativeUtilityFunctionGenerator.kt
+++ codegen/src/main/kotlin/io/github/kingg22/godot/codegen/impl/extensionapi/native/generators/NativeUtilityFunctionGenerator.kt
@@ -2,46 +2,36 @@ package io.github.kingg22.godot.codegen.impl.extensionapi.native.generators
 
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.UNIT
 import io.github.kingg22.godot.codegen.impl.createFile
 import io.github.kingg22.godot.codegen.impl.extensionapi.Context
-import io.github.kingg22.godot.codegen.impl.extensionapi.TypeResolver
 import io.github.kingg22.godot.codegen.impl.withExceptionContext
 import io.github.kingg22.godot.codegen.models.extensionapi.UtilityFunction
 
 /** Generates the `GD` object containing Godot utility functions. */
-class NativeUtilityFunctionGenerator(
-    private val typeResolver: TypeResolver,
-    private val methodGen: NativeMethodGenerator,
-) {
+class NativeUtilityFunctionGenerator(private val methodGen: NativeMethodGenerator) {
+
     context(context: Context)
     fun generateFile(functions: List<UtilityFunction>): FileSpec {
         val spec = generateSpec(functions)
         return createFile(spec, spec.name!!, context.packageForUtilObject())
     }
 
-    context(context: Context)
+    context(_: Context)
     fun generateSpec(functions: List<UtilityFunction>): TypeSpec {
         withExceptionContext({ "Generating utility functions, count: ${functions.size}" }) {
-            val gdTypeSpec = TypeSpec
-                .objectBuilder("GD")
-                .addKdoc("Utility functions for Godot API.")
-
-            functions.forEach { fn ->
+            val functionsSpec = functions.map { fn ->
                 withExceptionContext({ "Error generating utility function '${fn.name}'" }) {
-                    val returnType = fn.returnType?.let { typeResolver.resolve(it) } ?: UNIT
-                    val funSpec = methodGen.buildMethod(
-                        name = fn.name,
-                        isVararg = fn.isVararg,
-                        returnType = returnType,
-                        arguments = fn.arguments,
-                        methodKdoc = fn.description?.plus("\nCategory: ${fn.category}") ?: "Category: ${fn.category}",
-                        className = "GD",
-                    )
-                    gdTypeSpec.addFunction(funSpec)
+                    methodGen.buildMethod(method = fn, className = "GD") {
+                        addKdoc("\n**Category**: %S", fn.category)
+                    }
                 }
             }
-            return gdTypeSpec.build()
+
+            return TypeSpec
+                .objectBuilder("GD")
+                .addKdoc("Utility functions for Godot API.")
+                .addFunctions(functionsSpec)
+                .build()
         }
     }
 }
