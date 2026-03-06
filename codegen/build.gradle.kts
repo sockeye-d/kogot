a4450647201fe64a726c8704ce39d90177276aeb
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-05 19:27:27 -0500

    build(codegen): disable some jvm compiler arguments for Kotlin

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -6,7 +6,17 @@ plugins {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xcontext-parameters")
+        freeCompilerArgs.addAll(
+            "-Xcontext-parameters",
+            "-Xno-param-assertions",
+            "-Xno-call-assertions",
+            "-Xno-receiver-assertions",
+            "-Xtype-enhancement-improvements-strict-mode",
+            "-Xwhen-expressions=indy",
+        )
+        // The Kotlin Compiler adds intrinsic assertions which are only relevant
+        // when the code is consumed by Java users. Therefore, we can turn this off
+        // when code is being consumed by Kotlin users.
     }
 }
 
