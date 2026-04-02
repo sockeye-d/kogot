fc62b521c893d67eb9510c81e5db12f1d4f21dc8
fish <me@fishies.dev>
2026-04-02 03:04:55 -0700

    Class testing
    # Conflicts:
    #	kotlin-native/sample/src/nativeMain/kotlin/io/github/kingg22/godot/sample/Main.kt
    
    (cherry picked from commit 70577038c7fdee62a0617ae79359c920c100dc81)

---
+++ kotlin-native/sample/src/nativeMain/kotlin/io/github/kingg22/godot/sample/CustomClass.kt
@@ -0,0 +1,8 @@
+package io.github.kingg22.godot.sample
+
+import io.github.kingg22.godot.api.core.Node
+import kotlinx.cinterop.COpaquePointer
+
+class CustomClass(nativePtr: COpaquePointer) : Node(nativePtr) {
+
+}
--- kotlin-native/sample/src/nativeMain/kotlin/io/github/kingg22/godot/sample/Main.kt
+++ kotlin-native/sample/src/nativeMain/kotlin/io/github/kingg22/godot/sample/Main.kt
@@ -1,18 +1,66 @@
 package io.github.kingg22.godot.sample
 
+import io.github.kingg22.godot.api.builtin.asStringName
+import io.github.kingg22.godot.api.singleton.ClassDB
 import io.github.kingg22.godot.api.utils.GD
 import io.github.kingg22.godot.api.utils.print
 import io.github.kingg22.godot.internal.binding.BindingProcAddressHolder
+import io.github.kingg22.godot.internal.binding.ClassDBBinding
+import io.github.kingg22.godot.internal.binding.ObjectBinding
 import io.github.kingg22.godot.internal.ffi.GDExtensionBool
+import io.github.kingg22.godot.internal.ffi.GDExtensionClassCreationInfo5
 import io.github.kingg22.godot.internal.ffi.GDExtensionClassLibraryPtr
 import io.github.kingg22.godot.internal.ffi.GDExtensionInitialization
 import io.github.kingg22.godot.internal.ffi.GDExtensionInitializationLevel
+import io.github.kingg22.godot.internal.ffi.GDExtensionInstanceBindingCallbacks
 import io.github.kingg22.godot.internal.ffi.GDExtensionInterfaceGetProcAddress
 import io.github.kingg22.godot.internal.ffi.TRUE
-import kotlinx.cinterop.COpaquePointer
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.pointed
-import kotlinx.cinterop.staticCFunction
+import kotlinx.cinterop.*
+import platform.posix.uint8_t
+
+private lateinit var globalLibrary: GDExtensionClassLibraryPtr
+
+val objectStorage = mutableMapOf<COpaquePointer, StableRef<*>>()
+
+val createInstanceFunc: CPointer<CFunction<(COpaquePointer?, uint8_t) -> COpaquePointer?>> = staticCFunction { _, _ ->
+    println("Create instance func called")
+    val base = ClassDB.instance.instantiate("Node".asStringName()).asObject().rawPtr
+    println("Base constructed")
+    val self = CustomClass(base)
+    println("Wrapper constructed")
+    val selfRef = StableRef.create(self)
+    println("Ref created")
+    val selfPtr = selfRef.asCPointer()
+    objectStorage[selfPtr] = selfRef
+    println("selfPtr created")
+    ObjectBinding.instance.setInstanceRaw(
+        base,
+        "CustomClass".asStringName().rawPtr,
+        selfPtr
+    )
+    println("setInstance")
+    ObjectBinding.instance.setInstanceBindingRaw(
+        pO = base,
+        pToken = globalLibrary,
+        pBinding = selfPtr,
+        pCallbacks = memScoped {
+            println("Making callbacks")
+            cValue<GDExtensionInstanceBindingCallbacks> {
+                println("Making callbacks, inside")
+                create_callback = null
+                free_callback = null
+                reference_callback = null
+            }.ptr
+        }
+    )
+    println("setInstanceBinding")
+    selfPtr
+}
+
+val freeInstanceFunc: CPointer<CFunction<(CPointer<out CPointed>?, CPointer<out CPointed>?) -> Unit>> = staticCFunction { _, ptr ->
+    println("Freeing $ptr")
+    objectStorage.remove(ptr)?.dispose()
+}
 
 @Suppress("unused") // Invoked by Godot
 @CName("godot_kotlin_init")
@@ -29,6 +77,8 @@ fun godotKotlinInit(
     initialization.userdata = null
     initialization.minimum_initialization_level = GDEXTENSION_INITIALIZATION_SCENE
 
+    globalLibrary = pLibrary
+
     return GDExtensionBool.TRUE
 }
 
@@ -38,9 +88,43 @@ private fun initialize(userdata: COpaquePointer?, level: GDExtensionInitializati
 
         GDEXTENSION_INITIALIZATION_SERVERS -> println("✓ SERVERS initialized")
 
-        GDEXTENSION_INITIALIZATION_SCENE -> println("✓ SCENE initialized")
+        GDEXTENSION_INITIALIZATION_SCENE -> {
+            println("✓ SCENE initialized")
+            val info = cValue<GDExtensionClassCreationInfo5> {
+                is_virtual = false.toByte().toUByte()
+                is_abstract = false.toByte().toUByte()
+                is_exposed = true.toByte().toUByte()
+                set_func = null
+                get_func = null
+                get_property_list_func = null
+                free_property_list_func = null
+                property_can_revert_func = null
+                property_get_revert_func = null
+                validate_property_func = null
+                notification_func = null
+                to_string_func = null
+                reference_func = null
+                unreference_func = null
+                recreate_instance_func = null
+                get_virtual_func = null
+                get_virtual_call_data_func = null
+                call_virtual_with_data_func = null
+                class_userdata = null
+                create_instance_func = createInstanceFunc
+                free_instance_func = freeInstanceFunc
+            }
+            memScoped {
+                ClassDBBinding.instance.registerExtensionClass5Raw(
+                    globalLibrary,
+                    "CustomClass".asStringName().rawPtr,
+                    "Node".asStringName().rawPtr,
+                    info.ptr,
+                )
+            }
+            println("Hi")
+        }
 
-        GDEXTENSION_INITIALIZATION_EDITOR -> GD.print("✓ EDITOR initialized. Hello from Kotlin Native")
+        GDExtensionInitializationLevel.GDEXTENSION_INITIALIZATION_EDITOR -> GD.print("✓ EDITOR initialized. Hello from Kotlin Native")
 
         // False positive https://youtrack.jetbrains.com/issue/KT-77521
         else -> println("Unexpected $level, userdata: $userdata")
